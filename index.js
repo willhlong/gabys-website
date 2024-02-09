58c0a39a77cc676b85396dc3539110f5b4629aa2
William Long <willhlong@gmail.com>
2024-02-08 23:43:42 -0500

    randomly fetches an image from dogceo.com

--- index.js
+++ index.js
@@ -5,22 +5,18 @@ const apiUrl = "https://dog.ceo/api/breed/retriever/golden/images/random";
 function fetchImage() {
     fetch(apiUrl)
     .then((response) => {
-        console.log(response);
         if(!response.ok) {
             throw new Error('Error retrieving image from API');
         }
-        response.json();
-        console.log(response);
+        return response.json();
     })
-    // .then (blob => {
-    //     const imageUrl = URL.createObjectURL(blob);
-    //     const el = document.getElementById('image-container');
-    //     const image = document.createElement('img');
-    //     image.src = imageUrl;
-    //     console.log(image.src);
-    //     el.appendChild(image);
-    // })
+    .then (data => {
+        const el = document.getElementById('image-container');
+        const image = document.createElement('img');
+        image.src = data.message;
+        el.appendChild(image);
+    })
     .catch((error) => console.error(error));
   }
 
-  fetchImage();
+  
